e02bd8e28732e6594c4e643dab156096ace82e05
Stephen Wong <yummywongttv@gmail.com>
2021-11-18 14:01:09 -0500

    fixed 404 errors and added gitignore

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ProductList from "../components/ProductList";
-import CategoryMenu from "../components/CategoryMenu";
+import CategoryMenu from "../components/CartegoryMenu";
 import Cart from '../components/Cart';
 
 const Home = () => {
