0ac110deb28257c4d52cb2582838a67082e26e40
Stephen Wong <yummywongttv@gmail.com>
2021-11-18 13:43:26 -0500

    made files

--- client/src/components/CartItem/index.js
+++ client/src/components/CartItem/index.js
@@ -6,69 +6,66 @@ import { useDispatch } from 'react-redux';
 
 const CartItem = ({ item }) => {
     // const [dispatch] = useStoreContext();
-    const dispatch = useDispatch();
+    const dispatch = useDispatch(); 
 
     const removeFromCart = item => {
         dispatch({
-            type: REMOVE_FROM_CART,
-            _id: item._id
+          type: REMOVE_FROM_CART,
+          _id: item._id
         });
-        idbPromise('cart', 'delete', {...item });
-    };
+        idbPromise('cart', 'delete', { ...item });
+      };
 
     const onChange = (e) => {
         const value = e.target.value;
-
+      
         if (value === '0') {
             dispatch({
-                type: REMOVE_FROM_CART,
-                _id: item._id
+              type: REMOVE_FROM_CART,
+              _id: item._id
             });
-
-            idbPromise('cart', 'delete', {...item });
-        } else {
+          
+            idbPromise('cart', 'delete', { ...item });
+          } else {
             dispatch({
-                type: UPDATE_CART_QUANTITY,
-                _id: item._id,
-                purchaseQuantity: parseInt(value)
+              type: UPDATE_CART_QUANTITY,
+              _id: item._id,
+              purchaseQuantity: parseInt(value)
             });
+          
+            idbPromise('cart', 'put', { ...item, purchaseQuantity: parseInt(value) });
+          }
+      };
 
-            idbPromise('cart', 'put', {...item, purchaseQuantity: parseInt(value) });
-        }
-    };
-
-    return ( <
-        div className = "flex-row" >
-        <
-        div >
-        <
-        img src = { `/images/${item.image}` }
-        alt = "" /
-        >
-        <
-        /div> <
-        div >
-        <
-        div > { item.name }, $ { item.price } < /div> <
-        div >
-        <
-        span > Qty: < /span> <
-        input type = "number"
-        placeholder = "1"
-        value = { item.purchaseQuantity }
-        onChange = { onChange }
-        /> <
-        span role = "img"
-        aria - label = "trash"
-        onClick = {
-            () => removeFromCart(item) } >
-        🗑️
-        <
-        /span> <
-        /div> <
-        /div> <
-        /div>
-    );
+  return (
+    <div className="flex-row">
+      <div>
+        <img
+          src={`/images/${item.image}`}
+          alt=""
+        />
+      </div>
+      <div>
+        <div>{item.name}, ${item.price}</div>
+        <div>
+          <span>Qty:</span>
+          <input
+                type="number"
+                placeholder="1"
+                value={item.purchaseQuantity}
+                onChange={onChange}
+                />
+          <span
+                role="img"
+                aria-label="trash"
+                onClick={() => removeFromCart(item)}
+                >
+                🗑️
+                </span>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default CartItem;
